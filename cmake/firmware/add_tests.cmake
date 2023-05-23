16ceac482b2a9ededb6e92083e1eb9c3cfa851d5
Risto Pejasinovic <risto.pejasinovic@cern.ch>
2023-05-23 10:17:31 +0200

    support different hex file widths for add test

--- cmake/firmware/add_tests.cmake
+++ cmake/firmware/add_tests.cmake
@@ -1,17 +1,24 @@
 function(add_tests EXECUTABLE DIRECTORY)
-    cmake_parse_arguments(ARG "" "" "ARGS;DEPS" ${ARGN})
+    cmake_parse_arguments(ARG "" "WIDTH" "ARGS;DEPS" ${ARGN})
+    if(ARG_UNPARSED_ARGUMENTS)
+        message(FATAL_ERROR "${CMAKE_CURRENT_FUNCTION} passed unrecognized argument " "${ARG_UNPARSED_ARGUMENTS}")
+    endif()
      
     include("${CMAKE_CURRENT_FUNCTION_LIST_DIR}/../utils/subdirectory_search.cmake")
     include("${CMAKE_CURRENT_FUNCTION_LIST_DIR}/fw_utils.cmake")
     SUBDIRLIST(TEST_SUBDIRS ${DIRECTORY})
+
+    if(NOT ARG_WIDTH)
+        set(ARG_WIDTH 32)
+    endif()
     
     enable_testing()
     foreach(test ${TEST_SUBDIRS})
         add_subdirectory("${DIRECTORY}/${test}" "${test}_test")
         foreach(fw_prj ${FW_PROJECT_NAME})
-            get_target_property(HEX_FILE ${fw_prj} HEX_32bit_FILE)
-            get_target_property(HEX_TEXT_FILE ${fw_prj} HEX_TEXT_32bit_FILE)
-            get_target_property(HEX_DATA_FILE ${fw_prj} HEX_DATA_32bit_FILE)
+            get_target_property(HEX_FILE ${fw_prj} HEX_${ARG_WIDTH}bit_FILE)
+            get_target_property(HEX_TEXT_FILE ${fw_prj} HEX_TEXT_${ARG_WIDTH}bit_FILE)
+            get_target_property(HEX_DATA_FILE ${fw_prj} HEX_DATA_${ARG_WIDTH}bit_FILE)
             add_test(
                 NAME ${fw_prj}
                 COMMAND ./${EXECUTABLE} 
