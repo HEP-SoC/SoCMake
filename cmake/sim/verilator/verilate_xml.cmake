376018e0ffa8f9d4fd2ffb0c5d4063b4c4282623
Anvesh Nookala <anvesh.nookala@cern.ch>
2023-08-09 00:29:14 +0200

    update verilate_xml with ip_lib functions

--- cmake/sim/verilator/verilate_xml.cmake
+++ cmake/sim/verilator/verilate_xml.cmake
@@ -1,142 +1,114 @@
 include_guard(GLOBAL)
 
-function(_verilate_xml TARGET)
-    set(OPTIONS "EXCLUDE_FROM_ALL")
-    set(ONE_PARAM_ARGS "PREFIX;TOP_MODULE;DIRECTORY")
-    set(MULTI_PARAM_ARGS "SOURCES;INCLUDE_DIRS")
-
-    cmake_parse_arguments(VERILATE "${OPTIONS}"
-        "${ONE_PARAM_ARGS}"
-        "${MULTI_PARAM_ARGS}"
-        ${ARGN})
-
-    foreach(inc_dir ${INTERFACE_INCLUDE_DIRECTORIES} ${INCLUDE_DIRECTORIES})
-        list(APPEND INC_DIRS_ARG -I${inc_dir})
-    endforeach()
-
-    set(STAMP_FILE "${BINARY_DIR}/${RTLLIB}_${CMAKE_CURRENT_FUNCTION}.stamp")
-    add_custom_command(
-        OUTPUT 
-        COMMAND
-        )
-    add_custom_target(xml
-        COMMAND verilator --xml-only 
-        ${SOURCES}
-        ${INC_DIRS_ARG}
-        )
-    
-endfunction()
-
-
-function(verilate_xml RTLLIB)
-    set(OPTIONS "")
-    set(ONE_PARAM_ARGS "PREFIX;TOP_MODULE;DIRECTORY;VERILATOR_ARGS")
-    set(MULTI_PARAM_ARGS "")
-
-    cmake_parse_arguments(VERILATE "${OPTIONS}"
-        "${ONE_PARAM_ARGS}"
-        "${MULTI_PARAM_ARGS}"
-        ${ARGN})
-
-    include("${CMAKE_CURRENT_FUNCTION_LIST_DIR}/../../rtllib.cmake")
-
-    get_target_property(BINARY_DIR ${RTLLIB} BINARY_DIR)
-
-    get_rtl_target_property(INTERFACE_INCLUDE_DIRECTORIES ${RTLLIB} INTERFACE_INCLUDE_DIRECTORIES)
-    get_rtl_target_property(INCLUDE_DIRECTORIES ${RTLLIB} INCLUDE_DIRECTORIES)
-    foreach(INC_DIR ${INTERFACE_INCLUDE_DIRECTORIES} ${INCLUDE_DIRECTORIES})
-        list(APPEND INC_DIRS_ARG -I${INC_DIR})
-    endforeach()
-
-    if(NOT VERILATE_DIRECTORY)
-        set(VERILATE_DIRECTORY ${BINARY_DIR}/vlt_xml)
-    endif()
-
-    if(VERILATE_TOP_MODULE)
-        set(TOP_MODULE_ARG --top-module ${VERILATE_TOP_MODULE})
-    endif()
-
-    get_rtl_target_property(LIB_VERILATOR_ARGS ${RTLLIB} VERILATOR_ARGS)
-
-    get_rtl_target_sources(V_FILES ${RTLLIB} SOURCES)
-    list(REMOVE_DUPLICATES V_FILES)
-
-    file(MAKE_DIRECTORY ${VERILATE_DIRECTORY})
-
-    set(XML_FILE ${VERILATE_DIRECTORY}/${RTLLIB}.xml)
-    set_source_files_properties(${XML_FILE} PROPERTIES GENERATED TRUE)
-    set(STAMP_FILE "${OUTDIR}/${RTLLIB}_${CMAKE_CURRENT_FUNCTION}.stamp")
-    add_custom_command(
-        OUTPUT ${XML_FILE} ${STAMP_FILE}
-        COMMAND verilator --xml-output ${XML_FILE}
-        ${V_FILES}
-        ${INC_DIRS_ARG}
-        ${TOP_MODULE_ARG} ${VERILATE_VERILATOR_ARGS} ${LIB_VERILATOR_ARGS}
-        COMMAND touch ${STAMP_FILE}
-        DEPENDS ${V_FILES}
-        )
-
-    add_custom_target(
-        ${RTLLIB}_vlt_xml
-        DEPENDS ${XML_FILE} ${STAMP_FILE} ${V_FILES} ${RTLLIB}
-        )
-endfunction()
-
-function(create_vlt_regpublic RTLLIB)
-    set(OPTIONS "")
-    set(ONE_PARAM_ARGS "OUTDIR;TOP_MODULE")
-    set(MULTI_PARAM_ARGS "")
+function(verilate_xml IP_LIB)
+    set(OPTIONS "GEN_REGPUBLIC")
+    set(ONE_PARAM_ARGS "PREFIX;OUTDIR;TOP_MODULE")
+    set(MULTI_PARAM_ARGS "VERILATOR_ARGS")
 
     cmake_parse_arguments(ARG "${OPTIONS}"
         "${ONE_PARAM_ARGS}"
         "${MULTI_PARAM_ARGS}"
         ${ARGN})
 
-    include("${CMAKE_CURRENT_FUNCTION_LIST_DIR}/rtllib.cmake")
+    if(ARG_UNPARSED_ARGUMENTS)
+        message(FATAL_ERROR "${CMAKE_CURRENT_FUNCTION} passed unrecognized argument " "${ARG_UNPARSED_ARGUMENTS}")
+    endif()
 
-    get_target_property(BINARY_DIR ${RTLLIB} BINARY_DIR)
+    include("${CMAKE_CURRENT_FUNCTION_LIST_DIR}/../../hwip.cmake")
+    include("${CMAKE_CURRENT_FUNCTION_LIST_DIR}/../../utils/find_python.cmake")
+
+    # frpm hwip.cmake. deferences full name of ip
+    ip_assume_last(IP_LIB ${IP_LIB})
+    
+    # get binary dir location
+    get_target_property(BINARY_DIR ${IP_LIB} BINARY_DIR)
 
     if(NOT ARG_OUTDIR)
-        set(ARG_OUTDIR ${BINARY_DIR}/regpublic)
+        set(OUTDIR ${BINARY_DIR}/verilate_xml)
+    else()
+        set(OUTDIR ${ARG_OUTDIR})
     endif()
+    file(MAKE_DIRECTORY ${OUTDIR})
+
+    get_ip_include_directories(INCLUDE_DIRS ${IP_LIB})
+
+    foreach(INC_DIR ${INCLUDE_DIRS})
+        list(APPEND INC_DIRS_ARG -I${INC_DIR})
+    endforeach()
 
     if(ARG_TOP_MODULE)
         set(TOP_MODULE ${ARG_TOP_MODULE})
     else()
-        set(TOP_MODULE ${RTLLIB})
+        get_target_property(TOP_MODULE ${IP_LIB} IP_NAME)
     endif()
 
-    file(MAKE_DIRECTORY ${ARG_OUTDIR})
+    get_ip_property(VERILATOR_ARGS ${IP_LIB} VERILATOR_ARGS)
+    list(APPEND VERILATOR_ARGS ${ARG_VERILATOR_ARGS})
 
-    set(XML_FILE ${ARG_OUTDIR}/${RTLLIB}.xml)
-    verilator_xml(${RTLLIB}
-        TOP_MODULE ${TOP_MODULE}
-        VERILATOR_ARGS --Wno-fatal ${VERILATOR_ARGS}
-        DIRECTORY ${ARG_OUTDIR}
-        )
+    get_ip_sources(V_SOURCES ${IP_LIB} VERILOG)          # TODO make merge source files group function
+    get_ip_sources(SOURCES ${IP_LIB} SYSTEMVERILOG)
+    list(PREPEND SOURCES ${V_SOURCES})
 
-    set(VLT_CFG_FILE ${ARG_OUTDIR}/${RTLLIB}_regpublic.vlt)
-    set(REG_LIST ${ARG_OUTDIR}/${RTLLIB}_reglist.txt)
-    set(REG_H_FILE ${ARG_OUTDIR}/${RTLLIB}_seq_vpi_handles.h)
-    set_source_files_properties(${VLT_CFG_FILE} PROPERTIES GENERATED TRUE)
+    if(NOT SOURCES)
+        message(FATAL_ERROR "Verilate function needs at least one VERILOG or SYSTEMVERILOG source added to the IP")
+    endif()
 
-    set(STAMP_FILE "${BINARY_DIR}/${RTLLIB}_${CMAKE_CURRENT_FUNCTION}.stamp")
+    set(XML_FILE ${OUTDIR}/${IP_LIB}.xml)
+
+    # taken from verilator github cmake file
+    find_program(VERILATOR_BIN NAMES verilator_bin
+      HINTS ${VERISC_HOME}/open/* $ENV{VERISC_HOME}/open/* ${VERILATOR_HOME}/bin ENV VERILATOR_HOME
+      NO_CMAKE_PATH NO_CMAKE_ENVIRONMENT_PATH NO_CMAKE_SYSTEM_PATH)
+
+    set(STAMP_FILE "${OUTDIR}/${IP_LIB}_${CMAKE_CURRENT_FUNCTION}.stamp")
+
     add_custom_command(
-        OUTPUT ${VLT_CFG_FILE} ${REG_LIST} ${REG_H_FILE} ${STAMP_FILE}
-        COMMAND python3 ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/scripts/parse_vlt_xml.py
-        ${XML_FILE}
-        ${ARG_OUTDIR}
-        --prefix ${RTLLIB}
-        --vlt --reg-list --reg-h
+        OUTPUT ${XML_FILE} ${STAMP_FILE}
+        COMMAND ${VERILATOR_BIN} --xml-output ${XML_FILE}
+        ${SOURCES}
+        ${INC_DIRS_ARG}
+        ${VERILATOR_ARGS}
+        --top-module ${TOP_MODULE}
         COMMAND touch ${STAMP_FILE}
-        DEPENDS ${RTLLIB}_vlt_xml ${XML_FILE}
+        DEPENDS ${SOURCES}
         )
 
     add_custom_target(
-        ${RTLLIB}_regpublic
-        DEPENDS ${VLT_CFG_FILE} ${REG_LIST} ${REG_H_FILE} ${STAMP_FILE} ${XML_FILE}
+        ${IP_LIB}_verilate_xml
+        DEPENDS ${XML_FILE} ${STAMP_FILE} ${IP_LIB}
         )
 
-    target_sources(${RTLLIB} INTERFACE ${VLT_CFG_FILE})
-    target_include_directories(${RTLLIB} INTERFACE ${ARG_OUTDIR})
+    if(ARG_GEN_REGPUBLIC)
+      set(VLT_CFG_FILE ${OUTDIR}/${IP_LIB}_regpublic.vlt)
+      set(REG_LIST ${OUTDIR}/${IP_LIB}_reglist.txt)
+      set(REG_H_FILE ${OUTDIR}/${IP_LIB}_seq_vpi_handles.h)
+      set(STAMP_FILE "${BINARY_DIR}/${IP_LIB}_${CMAKE_CURRENT_FUNCTION}.stamp")
+      
+      find_python3() # sets Python3_EXECUTABLE 
+      add_custom_command(
+          OUTPUT ${VLT_CFG_FILE} ${REG_LIST} ${REG_H_FILE} ${STAMP_FILE}
+          COMMAND ${Python3_EXECUTABLE} ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/scripts/parse_vlt_xml.py
+          ${XML_FILE}
+          ${OUTDIR}
+          --prefix ${TOP_MODULE}
+          --vlt --reg-list --reg-h
+          COMMAND touch ${STAMP_FILE}
+          DEPENDS ${IP_LIB}_verilate_xml ${XML_FILE}
+          )
+
+      add_custom_target(
+          ${IP_LIB}_verilate_xml_regpublic
+          DEPENDS ${VLT_CFG_FILE} ${REG_LIST} ${REG_H_FILE} ${STAMP_FILE} ${XML_FILE}
+          )
+      
+      # add vlt file to sources list
+      ip_sources(${IP_LIB} SYSTEMVERILOG ${VLT_CFG_FILE})
+      target_sources(${IP_LIB} INTERFACE
+          FILE_SET HEADERS 
+          BASE_DIRS "${OUTDIR}"
+          FILES ${REG_H_FILE}
+          )
+      
+    endif()
+
 endfunction()
