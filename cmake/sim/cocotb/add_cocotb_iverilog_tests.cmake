3fcdba4ded06bd156be201e10b7a93ec5085a8a3
Benoît Denkinger <42833463+benoitdenkinger@users.noreply.github.com>
2024-08-20 11:12:09 +0200

    fix ctest check for pass and failure. (#20)
    Co-authored-by: Benoit Denkinger <benoit.denkinger@cern.ch>

--- cmake/sim/cocotb/add_cocotb_iverilog_tests.cmake
+++ cmake/sim/cocotb/add_cocotb_iverilog_tests.cmake
@@ -36,10 +36,9 @@ function(add_cocotb_iverilog_tests IP_LIB DIRECTORY)
                 set_property(TEST ${cocotb_test} APPEND PROPERTY ENVIRONMENT ${prop})
             endforeach()
             # vvp (iverilog) always returns 0 (pass) so check the output to detect a failure
-            # Also detect if python env is not correct otherwise we have a silent failure
-            # In general Error is not expected
+            # We only check if test passed or not checking cocotb summary header at the end
             set_property(TEST ${cocotb_test} PROPERTY
-                FAIL_REGULAR_EXPRESSION "[^a-z]FAIL;Error;ERROR;error"
+                PASS_REGULAR_EXPRESSION "[^a-z]FAIL=0"
             )
         endforeach()
     endforeach()
