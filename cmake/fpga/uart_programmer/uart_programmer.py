e96a3ce885d4ebed3aeb9832fb70137778d1ce0c
George Nikolaidis <georgen2003@gmail.com>
2024-09-03 12:17:42 +0300

    Added port selection to Uart programmer (#23)
    * Added port selection to Uart programmer
    
    * Better error handling

--- cmake/fpga/uart_programmer/uart_programmer.py
+++ cmake/fpga/uart_programmer/uart_programmer.py
@@ -1,5 +1,6 @@
 import serial
 import argparse
+import serial.tools.list_ports
 
 def program(
         text_hex  : str,
@@ -61,6 +62,35 @@ def program(
     print(f"----- Bytes written: Text {len(text_bytes)}, Data: {len(data_bytes)} --------")
     print("-----------------------------------------------------")
 
+def getport(dev):
+    ports = serial.tools.list_ports.comports()
+    port_names = [port.device for port in ports]
+    
+    default_port = port_names[0]
+    if dev in port_names:
+        default_port = dev
+    elif "/dev/ttyUSB0" in port_names:
+        default_port = "/dev/ttyUSB0"
+        
+    print(f"Available ports:")
+    for index,port in enumerate(port_names):
+        print(f"{index+1}: {port}", end="")
+        if port == default_port:
+            print(" (default)", end="")
+            
+        print()
+        
+    input_port = input(f"Enter port number (default {default_port}): ")
+    try:
+        port = port_names[int(input_port)-1]
+    except:
+        port = default_port
+        
+    print(f"Selected port: {port}")
+    return port
+        
+    
+
 def main():
     parser = argparse.ArgumentParser(
             prog='UART programmer',
@@ -73,11 +103,13 @@ def main():
 
     args = parser.parse_args()
 
+    dev = getport(args.dev)
+
     program(
             text_hex=args.text_hex,
             data_hex=args.data_hex,
             baudrate=args.baudrate,
-            dev=args.dev,
+            dev=dev,
             )
 
 if __name__ == "__main__":
