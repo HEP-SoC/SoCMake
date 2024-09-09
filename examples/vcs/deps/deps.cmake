ad00bd818e178595accb17491533793b42f0d3b3
Risto Pejašinović <risto.pejasinovic@gmail.com>
2024-09-09 19:16:58 +0200

    fix SoCMake examples git remote repository

--- examples/vcs/deps/deps.cmake
+++ examples/vcs/deps/deps.cmake
@@ -1,8 +1,8 @@
-set(CPM_DOWNLOAD_VERSION 0.38.1)
+set(CPM_DOWNLOAD_VERSION 0.40.2)
 include(${CMAKE_CURRENT_LIST_DIR}/CPM.cmake)
 
 CPMAddPackage(
     NAME SoCMake
     GIT_TAG master
-    GIT_REPOSITORY "ssh://git@gitlab.cern.ch:7999/socmake/SoCMake.git"
+    GIT_REPOSITORY "https://github.com/HEP-SoC/SoCMake.git"
     )
