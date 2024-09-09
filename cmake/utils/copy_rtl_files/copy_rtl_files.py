3685bedb8447f248b6561ced6241c6e722ab9afc
Benoît Denkinger <42833463+benoitdenkinger@users.noreply.github.com>
2024-09-09 10:42:08 +0200

    skiplist and top-module arguments default value set to None. (#30)
    Co-authored-by: Benoit Denkinger <benoit.denkinger@cern.ch>

--- cmake/utils/copy_rtl_files/copy_rtl_files.py
+++ cmake/utils/copy_rtl_files/copy_rtl_files.py
@@ -5,19 +5,19 @@ import os
 
 def make_parser():
     parser = argparse.ArgumentParser(description="Filter RTL files based on module hierarchy.")
-    parser.add_argument("--top-module", dest="top_module", action="store", default="", help="Specify top module name")
-    parser.add_argument("--skiplist", dest="skiplist", action="store", default="", help="Optional list of modules to skip")
+    parser.add_argument("--top-module", dest="top_module", action="store", default=None, help="Specify top module name")
+    parser.add_argument("--skiplist", dest="skiplist", action="store", default=None, help="Optional list of modules to skip")
     parser.add_argument("--deps_dir", dest="deps_dir", action="store", default="", help="Base directory of the dependencies")
     parser.add_argument("--include", dest="inc_dirs", action='append', type=str, help="Directories where to look for include files")
     parser.add_argument('--outdir', dest='outdir', action='store', default='.', help='Output directory where files will be copied')
     parser.add_argument('sources', metavar='FILE', nargs='+', type=str, help='List of RTL file paths')
     return parser
-    
+
 
 def main():
     parser = make_parser()
     args = parser.parse_args()
-    
+
     # Check if vhier is available
     try:
         vhier = shutil.which('vhier')
@@ -25,11 +25,11 @@ def main():
             raise FileNotFoundError
     except FileNotFoundError:
         print('Error: "vhier" executable not found')
-        
+
     # Initialize the output list with all the packages,
     # because they're not retained by vhier
     output_src = [f for f in args.sources if "_pkg" in f]
-    
+
     # Set common vhier arguments
     top_module = ('--top-module', args.top_module) if args.top_module is not None else ()
     skiplist = ('--skiplist', args.skiplist) if args.skiplist is not None else ()
@@ -41,7 +41,7 @@ def main():
         *[f'+incdir+{i}' for i in args.inc_dirs],
         *args.sources,
     ]
-    
+
     # Get the used files list from vhier
     try:
         cells_output = subprocess.run([*vhier_base_args, '--cells'], capture_output=True, check=True)
@@ -50,7 +50,7 @@ def main():
         raise
 
     output_src.extend(sorted(set([f.decode() for f in cells_output.stdout.split()])))
-    
+
     # Copy files to output directory
     copied_src = []
     for i in output_src:
@@ -71,7 +71,7 @@ def main():
         raise
 
     output_inc = sorted(set([f.decode() for f in includes_output.stdout.split()]))
-    
+
     # Copy the include files in a include folder
     dest_dir = os.path.join(args.outdir, 'include')
     os.makedirs(dest_dir, exist_ok=True)
