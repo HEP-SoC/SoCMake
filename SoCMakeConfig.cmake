c1e6238491b39af710461ab583d2874f9d592f3f
Risto Pejašinović <risto.pejasinovic@gmail.com>
2024-09-14 21:32:28 +0200

    Add support for vhier, generating file list, module list, xml file (#45)

--- SoCMakeConfig.cmake
+++ SoCMakeConfig.cmake
@@ -9,6 +9,7 @@ include("${CMAKE_CURRENT_LIST_DIR}/cmake/utils/graphviz.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/cmake/utils/multi_option.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/cmake/utils/find_python.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/cmake/utils/copy_rtl_files/copy_rtl_files.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/cmake/utils/copy_rtl_files/vhier.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/cmake/utils/print_list.cmake")
 
 # ====================================
