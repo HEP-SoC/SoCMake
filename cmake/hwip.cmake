532d0781c8ee2095dbd11021f7a3f1888ba00562
Risto Pejašinović <risto.pejasinovic@gmail.com>
2024-09-14 20:57:21 +0200

    allow add_ip short form, resolves #42 (#56)

--- cmake/hwip.cmake
+++ cmake/hwip.cmake
@@ -9,10 +9,32 @@ include("${CMAKE_CURRENT_LIST_DIR}/utils/safe_get_target_property.cmake")
 # This function is a wrapper around the cmake built-in
 # `add_library() <https://cmake.org/cmake/help/latest/command/add_library.html>`_ function.
 # It generates the library name using the vendor, library, name, and version (VLNV) information passed
-# in arguments (see get_ipname()). It creates two alias libraries to the default <vendor>__<library>__<name>__<version>:
-#
+# in arguments (see create_ip_vlnv()). It creates two alias libraries to the default <vendor>__<library>__<name>__<version>:
+# 
 # * <vendor>::<library>::<name>::<version> ('__' replaced by '::')
 # * <vendor>::<library>::<name> (short name without the version)
+#
+# This function can be used in FULL and SHORT form:
+# Full form:
+# ```
+# add_ip(ip
+#     VENDOR vendor
+#     LIBRARY lib
+#     VERSION 1.2.3
+#     )
+# ```
+# In full form it is possible to ommit VENDOR, LIBRARY and VERSION, although it is not recommended.
+#
+# Ommiting them all would have following signature:
+# ```
+# add_ip(ip2)
+# ```
+#
+# Short form:
+# ```
+# add_ip(vendor2::lib2::ip2::1.2.2)
+# ```
+# In short form only the full VLNV format is accepted
 #
 # :param IP_NAME: The name of the IP.
 # :type IP_NAME: string
@@ -27,24 +49,33 @@ include("${CMAKE_CURRENT_LIST_DIR}/utils/safe_get_target_property.cmake")
 # :type VERSION: string
 #]]
 function(add_ip IP_NAME)
-    cmake_parse_arguments(ARG "" "VERSION;DESCRIPTION;VENDOR;LIBRARY" "" ${ARGN})
+    cmake_parse_arguments(ARG "" "VENDOR;LIBRARY;VERSION;DESCRIPTION" "" ${ARGN})
 
     # Vendor and library arguments are expected at the minimum
     if(ARG_UNPARSED_ARGUMENTS)
         message(FATAL_ERROR "${CMAKE_CURRENT_FUNCTION} passed unrecognized argument "
                             "${ARG_UNPARSED_ARGUMENTS}")
     endif()
+    
+    # If none of optional arguments VENDOR, LIBRARY, VERSION are passed expect to receive VLNV format in IP_NAME like vendor::lib::ip::0.0.1
+    if(NOT ARG_VENDOR AND NOT ARG_LIBRARY AND NOT ARG_VERSION)
+        unset(ARG_VENDOR)
+        parse_ip_vlnv(${IP_NAME} VENDOR LIBRARY IP_NAME VERSION)
+        set(ARG_VENDOR ${VENDOR})
+        set(ARG_LIBRARY ${LIBRARY})
+        set(ARG_VERSION ${VERSION})
+    endif()
     # Issue a warning if one of the VLNV information is not passed (this triggers an error later anyway)
     if((NOT ARG_VERSION OR NOT ARG_VENDOR OR NOT ARG_LIBRARY) AND NOT SOCMAKE_NOWARN_VLNV)
-    message(WARNING "Consider using full VLNV format\n\
-                    IP block: ${IP_NAME}\n\
-                    VENDOR: ${ARG_VENDOR}\n\
-                    LIBRARY: ${ARG_LIBRARY}\n\
-                    VERSION: ${ARG_VERSION}")
+        message(WARNING "Consider using full VLNV format\n\
+                        IP block: ${IP_NAME}\n\
+                        VENDOR: ${ARG_VENDOR}\n\
+                        LIBRARY: ${ARG_LIBRARY}\n\
+                        VERSION: ${ARG_VERSION}")
     endif()
     # Create the IP unique name using VLNV information
-    get_ipname(IP_LIB ${IP_NAME} VENDOR "${ARG_VENDOR}" LIBRARY "${ARG_LIBRARY}" VERSION "${ARG_VERSION}")
-
+    create_ip_vlnv(IP_LIB ${IP_NAME} VENDOR "${ARG_VENDOR}" LIBRARY "${ARG_LIBRARY}" VERSION "${ARG_VERSION}")
+ 
     if(NOT TARGET ${IP_LIB})
         add_library(${IP_LIB} INTERFACE)
 
@@ -54,13 +85,20 @@ function(add_ip IP_NAME)
         endif()
 
         # TODO Maybe delete short name without version
-        get_ipname(IP_LIB_SHORT ${IP_NAME} VENDOR "${ARG_VENDOR}" LIBRARY "${ARG_LIBRARY}" VERSION "")
-        string(REPLACE "__" "::" ALIAS_NAME_SHORT "${IP_LIB_SHORT}")
-        if(NOT "${IP_LIB}" STREQUAL "${ALIAS_NAME_SHORT}")
-            add_library(${ALIAS_NAME_SHORT} ALIAS ${IP_LIB})
+        if(ARG_VERSION)
+            create_ip_vlnv(IP_LIB_SHORT ${IP_NAME} VENDOR "${ARG_VENDOR}" LIBRARY "${ARG_LIBRARY}" VERSION "")
+            string(REPLACE "__" "::" ALIAS_NAME_SHORT "${IP_LIB_SHORT}")
+            if(NOT "${IP_LIB}" STREQUAL "${ALIAS_NAME_SHORT}")
+                add_library(${ALIAS_NAME_SHORT} ALIAS ${IP_LIB})
+            endif()
         endif()
     endif()
 
+    # Unset the parent variables that might have been set by previous add_ip() call
+    unset(IP_VENDOR PARENT_SCOPE)
+    unset(IP_LIBRARY PARENT_SCOPE)
+    unset(IP_NAME PARENT_SCOPE)
+    unset(IP_VERSION PARENT_SCOPE)
     if(ARG_VENDOR)
         set(IP_VENDOR ${ARG_VENDOR} PARENT_SCOPE)
         set_target_properties(${IP_LIB} PROPERTIES VENDOR ${ARG_VENDOR})
@@ -101,7 +139,7 @@ endfunction()
 # :keyword VERSION: Version of the IP following a three-part version number (Major.Minor.Patch, e.g., 1.0.13).
 # :type VERSION: string
 #]]
-function(get_ipname OUTVAR IP_NAME)
+function(create_ip_vlnv OUTVAR IP_NAME)
     cmake_parse_arguments(ARG "" "VENDOR;LIBRARY;VERSION" "" ${ARGN})
     if(ARG_UNPARSED_ARGUMENTS)
         message(FATAL_ERROR "${CMAKE_CURRENT_FUNCTION} passed unrecognized argument " "${ARG_UNPARSED_ARGUMENTS}")
@@ -121,11 +159,73 @@ function(get_ipname OUTVAR IP_NAME)
     set(${OUTVAR} ${LIB_NAME} PARENT_SCOPE)
 endfunction()
 
+#[[[
+# This function parses IP name from the VLNV format e.g. (vendor::lib::ip::0.0.1)
+#
+# This functions appends the vendor, library, name, and version (VLNV) information separated by '__'
+# to create a unique string representing an IP name. This string is used as the library name when
+# when calling the cmake built-in
+# `add_library() <https://cmake.org/cmake/help/latest/command/add_library.html>`_ function (see add_ip()).
+#
+# :param OUTVAR: The generate IP name.
+# :type OUTVAR: string
+# :param IP_NAME: The name of the IP.
+# :type IP_NAME: string
+#
+# **Keyword Arguments**
+#
+# :keyword VENDOR: Name of the IP vendor.
+# :type VENDOR: string
+# :keyword LIBRARY: Name of the IP library.
+# :type LIBRARY: string
+# :keyword VERSION: Version of the IP following a three-part version number (Major.Minor.Patch, e.g., 1.0.13).
+# :type VERSION: string
+#]]
+function(parse_ip_vlnv IP_VLNV VENDOR LIBRARY IP_NAME VERSION)
+    message("IP_VLNV: ${IP_VLNV}")
+    cmake_parse_arguments(ARG "" "" "" ${ARGN})
+    if(ARG_UNPARSED_ARGUMENTS)
+        message(FATAL_ERROR "${CMAKE_CURRENT_FUNCTION} passed unrecognized argument " "${ARG_UNPARSED_ARGUMENTS}")
+    endif()
+
+    # Convert IP_VLNV into a list of tokens by replacing :: with ;
+    string(REPLACE "::" ";" IP_TOKENS ${IP_VLNV})
+    # Remove empty list elements in case something like vendor::::ip::1.2.3 is passed
+    list(REMOVE_ITEM IP_TOKENS "")
+
+    # Raise an error if there are different than 4 tokens provided (`add_ip(vendor::lib::ip::0.0.1)`), unless its only 1 (`add_ip(ip)`) 
+    list(LENGTH IP_TOKENS TOKEN_CNT)
+
+    # Its alowed for IP_VLNV to have 4 tokens (FULL) `add_ip(vendor::lib::ip::0.0.1)`
+    if(TOKEN_CNT EQUAL 4)
+        # Get elements of the list
+        list(GET IP_TOKENS 0 VENDOR)
+        list(GET IP_TOKENS 1 LIBRARY)
+        list(GET IP_TOKENS 2 IP_NAME)
+        list(GET IP_TOKENS 3 VERSION)
+    # Its alowed for IP_VLNV to have 1 token (SHORT) `add_ip(ip)`
+    elseif(TOKEN_CNT EQUAL 1)
+        set(IP_NAME ${IP_VLNV})
+        unset(VENDOR)
+        unset(LIBRARY)
+        unset(VERSION)
+    # Anything else is not allowed and will throw an error
+    else()
+        message(FATAL_ERROR "Please specify full VLNV format for IP: ${IP_VLNV}")
+    endif()
+
+    # Set output variables
+    set(VENDOR ${VENDOR} PARENT_SCOPE)
+    set(LIBRARY ${LIBRARY} PARENT_SCOPE)
+    set(IP_NAME ${IP_NAME} PARENT_SCOPE)
+    set(VERSION ${VERSION} PARENT_SCOPE)
+endfunction()
+
 # IS THIS REALLY NECESSARY?
 # If only IP name is given without full VLNV, assume rest from the project variables
 function(ip_assume_last VLNV IP_NAME) # TODO check SOURCE DIR if its the same as current
     if(NOT TARGET ${IP_NAME})
-        get_ipname(IP_LIB ${IP_NAME} VENDOR "${IP_VENDOR}" LIBRARY "${IP_LIBRARY}" VERSION "${IP_VERSION}")
+        create_ip_vlnv(IP_LIB ${IP_NAME} VENDOR "${IP_VENDOR}" LIBRARY "${IP_LIBRARY}" VERSION "${IP_VERSION}")
     endif()
     alias_dereference(IP_LIB ${IP_LIB})
     set(${VLNV} ${IP_LIB} PARENT_SCOPE)
