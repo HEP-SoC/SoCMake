18a0cb0a7ea3320a68d836a183fe00193cd95d2c
Risto Pejašinović <risto.pejasinovic@gmail.com>
2024-09-14 09:00:00 +0200

    finaly fixed?

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   favicon: 'img/SoCMakeLogo3.svg',
 
   // Set the production url of your site here
-  url: 'https://hep-soc.github.io/SoCMake/',
+  url: 'https://hep-soc.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl : '/SoCMake/',
