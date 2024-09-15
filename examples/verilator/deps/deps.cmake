3d6b93d8c07ee5e35bb54b598efac957080f2975
Risto Pejašinović <risto.pejasinovic@gmail.com>
2024-09-16 00:36:21 +0200

    rename verilate -> verilator, add description, refactor examples, add vhier description

--- examples/verilator/deps/deps.cmake
+++ examples/verilator/deps/deps.cmake
@@ -3,6 +3,6 @@ include(${CMAKE_CURRENT_LIST_DIR}/CPM.cmake) # Include the CPM.cmake downloader
 
 CPMAddPackage(              # Add SoCMake as a package
     NAME SoCMake
-    GIT_TAG master         # You can define GIT_TAG or VERSION for versioning
+    GIT_TAG develop         # You can define GIT_TAG or VERSION for versioning
     GIT_REPOSITORY "https://github.com/HEP-SoC/SoCMake.git"  # GIT_REPOSITORY or URL
     )
