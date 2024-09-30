7b7f816e0b80f5d31a1140c614bb7c79a9cb742d
Benoit Denkinger <benoit.denkinger@gmail.com>
2024-09-30 12:28:08 +0200

    Added formating macro for nice test help display.

--- cmake/firmware/add_tests.cmake
+++ cmake/firmware/add_tests.cmake
@@ -1,75 +1,5 @@
-function(format_string OUTPUT_VAR input_list replacements)
-    # Split the replacement characters into a list.
-    string(REPLACE ";" ";" replacement_list "${replacements}")
-
-    set(formatted_string "")   # To store the final formatted result
-    set(max_lengths "")        # To store the maximum length of each part before spaces
-
-    # Step 1: Calculate maximum lengths for the sections before each space
-    foreach(line IN LISTS input_list)
-        string(REPLACE " " ";" split_line "${line}")
-        set(index 0)
-        foreach(section IN LISTS split_line)
-            string(LENGTH "${section}" section_length)
-
-            # Expand max_lengths if it is the first pass
-            list(LENGTH max_lengths list_size)
-            if(list_size LESS_EQUAL index)
-                list(APPEND max_lengths 0)
-            endif()
-
-            # Update max length for each column section
-            list(GET max_lengths ${index} current_max_length)
-            if(section_length GREATER current_max_length)
-                list(REMOVE_AT max_lengths ${index})
-                list(INSERT max_lengths ${index} ${section_length})
-            endif()
-
-            math(EXPR index "${index} + 1")
-        endforeach()
-    endforeach()
-
-    # Step 2: Format each line based on the calculated max lengths
-    foreach(line IN LISTS input_list)
-        string(REPLACE " " ";" split_line "${line}")
-
-        set(formatted_line "")
-        set(index 0)
-        foreach(section IN LISTS split_line)
-            # Get the length of the section
-            string(LENGTH "${section}" section_length)
-
-            # Get the max length for this section
-            list(GET max_lengths ${index} max_length)
-
-            # Pad the section with spaces to match the maximum length
-            math(EXPR padding_length "${max_length} - ${section_length}")
-            string(REPEAT " " ${padding_length} padding)
-
-            # Append the section to the formatted line
-            set(formatted_line "${formatted_line}${section}${padding}")
-
-            # Add the replacement string if available, otherwise keep original space
-            list(LENGTH replacement_list replacement_count)
-            if(${index} LESS replacement_count)
-                list(GET replacement_list ${index} replacement_char)
-                set(formatted_line "${formatted_line}${replacement_char}")
-            else()
-                # Add original space back if no replacement is available
-                set(formatted_line "${formatted_line} ")
-            endif()
-
-            math(EXPR index "${index} + 1")
-        endforeach()
-
-        # Add the formatted line to the final formatted string
-        string(APPEND formatted_string "${formatted_line}\n")
-    endforeach()
-
-    # Step 3: Return the formatted string
-    set(${OUTPUT_VAR} "${formatted_string}" PARENT_SCOPE)
-endfunction()
-
+# Iterates through the DIRECTORY sub-directories and create targets
+# to start simulating each test.
 function(add_tests EXECUTABLE DIRECTORY)
     cmake_parse_arguments(ARG "USE_PLUSARGS" "WIDTH" "ARGS;DEPS" ${ARGN})
     if(ARG_UNPARSED_ARGUMENTS)
@@ -78,6 +8,7 @@ function(add_tests EXECUTABLE DIRECTORY)
 
     include("${CMAKE_CURRENT_FUNCTION_LIST_DIR}/../utils/subdirectory_search.cmake")
     include("${CMAKE_CURRENT_FUNCTION_LIST_DIR}/../utils/colours.cmake")
+    include("${CMAKE_CURRENT_FUNCTION_LIST_DIR}/../utils/format_string_spacing.cmake")
     include("${CMAKE_CURRENT_FUNCTION_LIST_DIR}/fw_utils.cmake")
     SUBDIRLIST(TEST_SUBDIRS ${DIRECTORY})
 
@@ -107,7 +38,7 @@ function(add_tests EXECUTABLE DIRECTORY)
             continue()
         endif()
         foreach(fw_prj ${FW_PROJECT_NAME})
-        list(APPEND _test_msg " ${fw_prj}: ${${fw_prj}_DESCRIPTION}\n")
+        list(APPEND _test_msg " ${fw_prj}: ${${fw_prj}_DESCRIPTION}")
             list(APPEND test_list ${fw_prj})
             get_target_property(HEX_FILE ${fw_prj} HEX_${ARG_WIDTH}bit_FILE)
             get_target_property(HEX_TEXT_FILE ${fw_prj} HEX_TEXT_${ARG_WIDTH}bit_FILE)
@@ -139,19 +70,14 @@ function(add_tests EXECUTABLE DIRECTORY)
         DEPENDS ${test_list} ${EXECUTABLE}
     )
 
-    # set(formatted_test_msg "")
-    format_string(formatted_test_msg ${_test_msg} "    ;  ")
-    # string(REPLACE ";" "" formatted_test_msg "${formatted_test_msg}")
-
-    message("Non-formatted string:\n${_test_msg}")
-    message("Formatted string:\n${formatted_test_msg}")
+    format_string_spacing(formatted_test_msg "${_test_msg}" "  ;  ")
 
     list(APPEND _msg "${formatted_test_msg}")
 
     list(APPEND _msg "\nTo run ctest on all of the tests run:\n")
-    list(APPEND _msg "    make check\n")
+    list(APPEND _msg "  make check\n")
     list(APPEND _msg "To run any of the added tests execute:\n")
-    list(APPEND _msg "   make run_<test_name>\n")
+    list(APPEND _msg "  make run_<test_name>\n")
     list(APPEND _msg "-------------------------------------------------------------------------")
     string(REPLACE ";" "" _msg "${_msg}")
     msg("${_msg}" Blue)
--- cmake/fpga/uart_programmer/uart_programmer.cmake
+++ cmake/fpga/uart_programmer/uart_programmer.cmake
@@ -1,79 +1,5 @@
-function(format_string OUTPUT_VAR input_string replacements)
-    # Split the input string into separate lines.
-    string(REPLACE "\n" ";" lines "${input_string}")
-
-    # Split the replacement characters into a list.
-    string(REPLACE ";" ";" replacement_list "${replacements}")
-
-    set(formatted_string "")   # To store the final formatted result
-    set(max_lengths "")        # To store the maximum length of each part before space
-
-    # Step 1: Calculate maximum lengths for the sections before each space
-    message("foreach(line IN LISTS lines): ${input_string}")
-    foreach(line IN LISTS lines)
-        message("${line}")
-        string(REPLACE " " ";" split_line "${line}")
-        set(index 0)
-        foreach(section IN LISTS split_line)
-            string(LENGTH "${section}" section_length)
-
-            # Expand max_lengths if it is the first pass
-            list(LENGTH max_lengths list_size)
-            if(list_size LESS_EQUAL index)
-                list(APPEND max_lengths 0)
-            endif()
-
-            # Update max length for each column section
-            list(GET max_lengths ${index} current_max_length)
-            if(section_length GREATER current_max_length)
-                list(REMOVE_AT max_lengths ${index})
-                list(INSERT max_lengths ${index} ${section_length})
-            endif()
-
-            math(EXPR index "${index} + 1")
-        endforeach()
-    endforeach()
-
-    # Step 2: Format each line based on the calculated max lengths
-    foreach(line IN LISTS lines)
-        string(REPLACE " " ";" split_line "${line}")
-
-        set(formatted_line "")
-
-        set(index 0)
-        foreach(section IN LISTS split_line)
-            # Get the length of the section
-            string(LENGTH "${section}" section_length)
-
-            # Get the max length for this section
-            list(GET max_lengths ${index} max_length)
-
-            # Pad the section with spaces to match the maximum length
-            math(EXPR padding_length "${max_length} - ${section_length}")
-            string(REPEAT " " ${padding_length} padding)
-
-            # Append the section to the formatted line
-            set(formatted_line "${formatted_line}${section}${padding}")
-
-            # Add the replacement string if available
-            list(LENGTH replacement_list replacement_count)
-            if(${index} LESS replacement_count)
-                list(GET replacement_list ${index} replacement_char)
-                set(formatted_line "${formatted_line}${replacement_char}")
-            endif()
-
-            math(EXPR index "${index} + 1")
-        endforeach()
-
-        # Add the formatted line to the final formatted string
-        string(APPEND formatted_string "${formatted_line}\n")
-    endforeach()
-
-    # Step 3: Return the formatted string
-    set(${OUTPUT_VAR} "${formatted_string}" PARENT_SCOPE)
-endfunction()
-
-
+# Iterates through the DIRECTORY sub-directories and create targets
+# to start uart transactions for each test.
 function(uart_programmer DIRECTORY BAUDRATE DEV)
     cmake_parse_arguments(ARG "" "" "" ${ARGN})
     if(ARG_UNPARSED_ARGUMENTS)
@@ -82,6 +8,7 @@ function(uart_programmer DIRECTORY BAUDRATE DEV)
 
     include("${CMAKE_CURRENT_FUNCTION_LIST_DIR}/../../utils/find_python.cmake")
     include("${CMAKE_CURRENT_FUNCTION_LIST_DIR}/../../utils/subdirectory_search.cmake")
+    include("${CMAKE_CURRENT_FUNCTION_LIST_DIR}/../../utils/format_string_spacing.cmake")
     include("${CMAKE_CURRENT_FUNCTION_LIST_DIR}/../../utils/colours.cmake")
     SUBDIRLIST(TEST_SUBDIRS ${DIRECTORY})
 
@@ -103,7 +30,7 @@ function(uart_programmer DIRECTORY BAUDRATE DEV)
             continue()
         endif()
         foreach(fw_prj ${FW_PROJECT_NAME})
-            list(APPEND _test_msg " ${fw_prj}: ${${fw_prj}_DESCRIPTION}\n")
+            list(APPEND _test_msg " ${fw_prj}: ${${fw_prj}_DESCRIPTION}")
             list(APPEND test_list ${fw_prj})
             get_target_property(HEX_FILE ${fw_prj} HEX_32bit_FILE)
             get_target_property(HEX_TEXT_FILE ${fw_prj} HEX_TEXT_32bit_FILE)
@@ -120,15 +47,12 @@ function(uart_programmer DIRECTORY BAUDRATE DEV)
         endforeach()
     endforeach()
 
-    format_string(formatted_test_msg "${_test_msg}" "    ;  ")
-
-    message("Non-formatted string:\n" ${_test_msg})
-    message("Formatted string:\n" ${formatted_test_msg})
+    format_string_spacing(formatted_test_msg "${_test_msg}" "  ;  ")
 
     list(APPEND _msg "${formatted_test_msg}")
 
     list(APPEND _msg "To run any of the added tests on the FPGA:\n")
-    list(APPEND _msg "   make program_<test_name>\n")
+    list(APPEND _msg "  make program_<test_name>\n")
     list(APPEND _msg "-------------------------------------------------------------------------")
     string(REPLACE ";" "" _msg "${_msg}")
     msg("${_msg}" Blue)
