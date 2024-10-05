1c6ae51743d86a61804fd4e88bbff1dc0bf87b53
Risto Pejašinović <risto.pejasinovic@gmail.com>
2024-10-05 23:02:57 +0200

    Peakrdl regblock refactor (#87)

--- cmake/peakrdl/peakrdl_regblock.cmake
+++ cmake/peakrdl/peakrdl_regblock.cmake
@@ -4,38 +4,40 @@
 # PeakRDL-regblock is transforming SystemRDL input files to SystemVerilog register block files.
 # Regblock documentation can be found on this `link <https://peakrdl-regblock.readthedocs.io/en/latest/>`_.
 #
-# Function expects that **${IP_LIB}** *INTERFACE_LIBRARY* has **SYSTEMRDL_SOURCES** property set with
+# Function expects that **${IP_LIB}** has **SYSTEMRDL_SOURCES** property set with
 # a list of SystemRDL files to be used as inputs. To set the SYSTEMRDL_SOURCES property use the ip_sources()
-# function from SoCMake (internally using `set_property()
-# <https://cmake.org/cmake/help/latest/command/set_property.html>`_ CMake function):
 #
 # .. code-block:: cmake
 #
-#    ip_sources(IP_LIB LANGUAGE [SYSTEMRDL|SYSTEMVERILOG|...] ${PROJECT_SOURCE_DIR}/file.rdl)
+#    ip_sources(ip SYSTEMRDL ${PROJECT_SOURCE_DIR}/file.rdl)
 #
 #
-# This function will append 2 generated files from PeakRDL-regblock to the **SOURCES** property of the
+# This function will append 2 generated files from PeakRDL-regblock to the **SYSTEMVERILOG_SOURCES** property of the
 # **${IP_LIB}**.
 #
-# :param IP_LIB: RTL interface library, it needs to have SYSTEMRDL_SOURCES property set with a list of
-# SystemRDL files.
-# :type IP_LIB: INTERFACE_LIBRARY
+# :param IP_LIB: IP for which to create regblock target.
+# :type IP_LIB: IP library
 #
 # **Keyword Arguments**
 #
 # :keyword OUTDIR: output directory in which the files will be generated.
 # If ommited ${BINARY_DIR}/regblock will be used.
-# :type OUTDIR: string path
+# :type OUTDIR: string
 # :keyword RENAME: Rename the generated module and file name to a custom string, otherwise the
 # name will be ${IP_LIB}.sv.
 # :type RENAME: string
 # :keyword INTF: Interface to use for the regblock. Possible values are:
 # [apb3 (default), apb3-flat, apb4, apb4-flat, axi4-lite, axi4-lite-flat, avalon-mm, avalon-mm-flat, passthrough]
 # :type INTF: string
+# :keyword RESET: reset type for generated regblock. Possible values are:
+# [rst (default), rst,rst_n,arst,arst_n]
+# :type RESET: string
+# :keyword ARGS: any additional arguments to pass to regblock cli executable
+# :type ARGS: list
 #]]
 function(peakrdl_regblock IP_LIB)
     # Parse keyword arguments
-    cmake_parse_arguments(ARG "" "OUTDIR;RENAME;INTF;RESET" "" ${ARGN})
+    cmake_parse_arguments(ARG "" "OUTDIR;RENAME;INTF;RESET" "ARGS" ${ARGN})
     # Check for any unknown argument
     if(ARG_UNPARSED_ARGUMENTS)
         message(FATAL_ERROR "${CMAKE_CURRENT_FUNCTION} passed unrecognized argument "
@@ -48,9 +50,8 @@ function(peakrdl_regblock IP_LIB)
     alias_dereference(IP_LIB ${IP_LIB})
     get_target_property(BINARY_DIR ${IP_LIB} BINARY_DIR)
 
-    # Default output directory is regblock/
     if(NOT ARG_OUTDIR)
-        set(OUTDIR ${BINARY_DIR}/regblock)
+        set(OUTDIR ${BINARY_DIR}/${IP_LIB}_${CMAKE_CURRENT_FUNCTION})
     else()
         set(OUTDIR ${ARG_OUTDIR})
     endif()
@@ -58,18 +59,14 @@ function(peakrdl_regblock IP_LIB)
     if(NOT ARG_RENAME)
         # The default name is the IP name
         get_target_property(REGBLOCK_NAME ${IP_LIB} IP_NAME)
-        if(NOT REGBLOCK_NAME)
-            message(FATAL_ERROR "IP_NAME not set for ${IP_LIB}, check if the IP was added with
-                    add_ip function from SoCMake")
-        endif()
         set(REGBLOCK_NAME ${REGBLOCK_NAME}_regblock)
     else()
         set(REGBLOCK_NAME ${ARG_RENAME})
     endif()
 
     # The default interface used is apb3, set another on if the argument exists
-    if(ARG_INTF)
-        set(INTF_ARG --cpuif ${ARG_INTF})
+    if(NOT ARG_INTF)
+        set(ARG_INTF apb3-flat)
     endif()
 
     # The default reset is active-high and synchronous
@@ -101,12 +98,13 @@ function(peakrdl_regblock IP_LIB)
     find_python3()
     set(__CMD ${Python3_EXECUTABLE} -m peakrdl regblock
             --rename ${REGBLOCK_NAME}
-            ${INTF_ARG}
+            --cpuif ${ARG_INTF}
             ${RESET_ARG}
             ${INCDIRS_ARG}
             ${COMPDEFS_ARG}
             -o ${OUTDIR}
             ${RDL_SOURCES}
+            ${ARG_ARGS}
         )
 
     set(SV_GEN
@@ -117,22 +115,21 @@ function(peakrdl_regblock IP_LIB)
     ip_sources(${IP_LIB} SYSTEMVERILOG PREPEND ${SV_GEN})
 
     set(STAMP_FILE "${BINARY_DIR}/${IP_LIB}_${CMAKE_CURRENT_FUNCTION}.stamp")
+    set(DESCRIPTION "Generate register file for \"${IP_LIB}\" with ${ARG_INTF} bus, with ${CMAKE_CURRENT_FUNCTION}")
     add_custom_command(
         # The output files are automtically marked as GENERATED (deleted by make clean among other things)
         OUTPUT ${SV_GEN} ${STAMP_FILE}
         COMMAND ${__CMD}
-
         COMMAND touch ${STAMP_FILE}
         DEPENDS ${RDL_SOURCES}
-        COMMENT "Running ${CMAKE_CURRENT_FUNCTION} on ${IP_LIB}"
+        COMMENT ${DESCRIPTION}
     )
     # This target triggers the systemverilog register block generation using peakRDL regblock tool (_CMD)
     add_custom_target(
-        ${IP_LIB}_regblock
+        ${IP_LIB}_${CMAKE_CURRENT_FUNCTION}
         DEPENDS ${SV_GEN} ${STAMP_FILE}
     )
-
-    add_dependencies(${IP_LIB} ${IP_LIB}_regblock)
-    set_property(TARGET ${IP_LIB} APPEND PROPERTY DEPENDS ${IP_LIB}_regblock)
+    set_property(TARGET ${IP_LIB}_${CMAKE_CURRENT_FUNCTION} PROPERTY DESCRIPTION ${DESCRIPTION})
+    add_dependencies(${IP_LIB} ${IP_LIB}_${CMAKE_CURRENT_FUNCTION})
 
 endfunction()
