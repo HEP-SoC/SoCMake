adf9bd8f273f4662eedff10a5a32bfe80d881351
Risto Pejašinović <risto.pejasinovic@gmail.com>
2024-10-06 09:01:17 +0200

    remove unused duplicate tests

--- tests/tests/iverilog_multi_top/top1.v
+++
@@ -1,6 +0,0 @@
-module top1;
-    initial begin
-        $display("Module top1\n");
-        $fatal();
-    end
-endmodule
--- tests/tests/iverilog_multi_top/top2.v
+++
@@ -1,7 +0,0 @@
-module top2;
-    initial begin
-        $display("Module top2\n");
-        $finish();
-    end
-endmodule
-
