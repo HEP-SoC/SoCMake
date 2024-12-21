3fc668222d31afda97da4ac4ead1e65e14a5f02d
Risto Pejašinović <risto.pejasinovic@gmail.com>
2024-12-22 00:22:12 +0100

    Refactor simulation examples
    Provided:
        * simple_verilog - Simple verilog only testbench for all simulators
        * simple_mixed_language - Verilog and VHDL tb for all capable sims
        * Remove modelsim and vcs examples

--- examples/sim_example/tb.v
+++
@@ -1,6 +0,0 @@
-module tb;
- initial begin
-     $display("Hello world, from SoCMake build system\n");
-     $finish();
- end
- endmodule
--- examples/vcs/deps/deps.cmake
+++
@@ -1,8 +0,0 @@
-set(CPM_DOWNLOAD_VERSION 0.40.2)
-include(${CMAKE_CURRENT_LIST_DIR}/CPM.cmake)
-
-CPMAddPackage(
-    NAME SoCMake
-    GIT_TAG master
-    GIT_REPOSITORY "https://github.com/HEP-SoC/SoCMake.git"
-    )
