272ee2511134596faf64e3cb1ca18a46cf98c018
Risto Pejašinović <risto.pejasinovic@gmail.com>
2025-04-30 08:46:20 +0200

    Update SoCMakeConfig.cmake

--- SoCMakeConfig.cmake
+++ SoCMakeConfig.cmake
@@ -118,3 +118,8 @@ include("${CMAKE_CURRENT_LIST_DIR}/cmake/riscv/sail/sail_install.cmake")
 # ====================================
 include("${CMAKE_CURRENT_LIST_DIR}/cmake/build_scripts/systemc/systemc_build.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/cmake/build_scripts/uvm-systemc/uvm-systemc_build.cmake")
+
+# ====================================
+# ====== IPXact ======================
+# ====================================
+include("${CMAKE_CURRENT_LIST_DIR}/cmake/ipxact/importer/ipxact_ip_importer.cmake")
