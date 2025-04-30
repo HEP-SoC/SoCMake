461871b334e15304579b0975b7c064103718fe55
Risto Pejašinović <risto.pejasinovic@gmail.com>
2025-04-30 10:54:34 +0200

    If xmlstarlet not available use xsltproc

--- cmake/ipxact/importer/ipxact_ip_importer.cmake
+++ cmake/ipxact/importer/ipxact_ip_importer.cmake
@@ -3,11 +3,18 @@ function(add_ip_from_ipxact COMP_XML)
     if(ARG_UNPARSED_ARGUMENTS)
         message(FATAL_ERROR "${CMAKE_CURRENT_FUNCTION} passed unrecognized argument " "${ARG_UNPARSED_ARGUMENTS}")
     endif()
+    
+    find_program(xml_tool xmlstarlet)
+    set(xml_command ${xml_tool} tr)
+    if(NOT xml_tool)
+        find_program(xml_tool xsltproc REQUIRED)
+        set(xml_command ${xml_tool})
+    endif()
 
     cmake_path(GET COMP_XML PARENT_PATH ip_source_dir)
     cmake_path(GET COMP_XML FILENAME file_name)
 
-    execute_process(COMMAND xmlstarlet tr "${CMAKE_CURRENT_FUNCTION_LIST_DIR}/get_vlnv.xslt" ${COMP_XML}
+    execute_process(COMMAND ${xml_command} "${CMAKE_CURRENT_FUNCTION_LIST_DIR}/get_vlnv.xslt" ${COMP_XML}
                     OUTPUT_VARIABLE vlnv_list)
     list(GET vlnv_list 0 ip_vendor)
     list(GET vlnv_list 1 ip_library)
@@ -16,11 +23,11 @@ function(add_ip_from_ipxact COMP_XML)
 
     set(output_cmake_file ${ip_source_dir}/${ip_vendor}_${ip_library}_${ip_name}_${ip_version}.cmake)
 
-    execute_process(COMMAND xmlstarlet tr "${CMAKE_CURRENT_FUNCTION_LIST_DIR}/ip_lib_with_filetype_modifier.xslt" ${COMP_XML}
+    execute_process(COMMAND ${xml_command} "${CMAKE_CURRENT_FUNCTION_LIST_DIR}/ip_lib_with_filetype_modifier.xslt" ${COMP_XML}
                     OUTPUT_VARIABLE file_lists
                 )
 
-    execute_process(COMMAND xmlstarlet tr "${CMAKE_CURRENT_FUNCTION_LIST_DIR}/get_ip_links.xslt" ${COMP_XML}
+    execute_process(COMMAND ${xml_command} "${CMAKE_CURRENT_FUNCTION_LIST_DIR}/get_ip_links.xslt" ${COMP_XML}
                     OUTPUT_VARIABLE ip_links
                 )
 
