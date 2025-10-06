15d16a758e64a361043b351173035f794356a585
Risto Pejašinović <risto.pejasinovic@gmail.com>
2025-10-07 01:40:09 +0200

    Big performance improvement by refactoring the topological sort to recursive DFS topsort

--- cmake/utils/socmake_graph.cmake
+++ cmake/utils/socmake_graph.cmake
@@ -5,115 +5,108 @@ include_guard(GLOBAL)
 # ========================================================== #
 
 function(flatten_graph NODE)
-    alias_dereference(NODE ${NODE}) # TODO provide set_property function that dereferences alias
+    alias_dereference(NODE ${NODE})
 
+    # __GLOBAL_STACK will hold the flattened graph as the DFS is traversing the tree
     set_property(GLOBAL PROPERTY __GLOBAL_STACK "")
-    set(FOUND_ROOT -1)
-    while(FOUND_ROOT EQUAL -1)
-        __flatten_graph_recursive(${NODE} FOUND_ROOT)
-        list(FIND STACK ${NODE} FOUND_ROOT)
-        get_property(STACK GLOBAL PROPERTY __GLOBAL_STACK)
-    endwhile()
+    # __ALREADY_VISITED flag marks that the node was already visited by the DFS search algorithm
+    # Since DFS visit every node once for a DAG, if a node is visited twice a cycle was detected
+    set_property(TARGET ${NODE} PROPERTY __ALREADY_VISITED FALSE)
+    # Once all the children of the node have been visited, mark this node as TRUE to avoid reentering it
+    set_property(TARGET ${NODE} PROPERTY __NODE_PROCESSED FALSE)
 
-    foreach(lib ${STACK}) # CLear __RM_LIST
+    # Recursive DFS topological sort
+    __dfs_topo(${NODE} unused)
+
+    get_property(STACK GLOBAL PROPERTY __GLOBAL_STACK)
+
+    foreach(lib ${STACK}) # Clear the temporary properties
         alias_dereference(lib ${lib})
-        set_property(TARGET ${lib} PROPERTY __RM_LIST "")
+        set_property(TARGET ${lib} PROPERTY __ALREADY_VISITED FALSE)
+        set_property(TARGET ${lib} PROPERTY __NODE_PROCESSED FALSE)
     endforeach()
-    set_property(TARGET ${NODE} PROPERTY FLAT_GRAPH ${STACK})
 
+    set_property(TARGET ${NODE} PROPERTY FLAT_GRAPH ${STACK})
 endfunction()
 
-function(__flatten_graph_recursive NODE RET)
+# ------------------------------------------------------------------ #
+# Recursive DFS topological sort
+# ------------------------------------------------------------------ #
+function(__dfs_topo NODE RET)
     alias_dereference(NODE ${NODE})
-    if(NOT TARGET ${NODE}) # If its not a target e.g. not interface library but maybe -pthread
-        set(${RET} 1 PARENT_SCOPE)
+
+    # Skip non-targets (like -pthread, etc.)
+    if(NOT TARGET ${NODE})
+        set(${RET} 0 PARENT_SCOPE)
         return()
     endif()
-    __all_vertices_removed(${NODE} ALL_VERTICES_REMOVED)
-    if(ALL_VERTICES_REMOVED EQUAL 1) # IF there is not DEPS on this node
-        
-        get_property(STACK GLOBAL PROPERTY __GLOBAL_STACK)
-        list(FIND STACK ${NODE} ALREADY_ADDED)
 
-        if(ALREADY_ADDED EQUAL -1)  # If the node is not in stack append it to stack
-            list(APPEND STACK ${NODE})
-            set_property(GLOBAL PROPERTY __GLOBAL_STACK ${STACK})
-        endif()
-
-        set(${RET} 1 PARENT_SCOPE)
+    # Already processed, just exit
+    get_target_property(processed ${NODE} __NODE_PROCESSED)
+    if(processed)
+        set(${RET} 0 PARENT_SCOPE)
         return()
     endif()
+
+    # Already visited, means there was a cycle detected, flag FATAL_ERROR
+    get_target_property(temp ${NODE} __ALREADY_VISITED)
+    if(temp)
+        message(FATAL_ERROR "Cycle detected in dependency graph at node: ${NODE}")
+    endif()
+
+    # Node is visited, we should not visit the same node again
+    set_property(TARGET ${NODE} PROPERTY __ALREADY_VISITED TRUE)
 
     get_target_property(LINK_LIBS ${NODE} INTERFACE_LINK_LIBRARIES)
-    if(LINK_LIBS STREQUAL "LINK_LIBS-NOTFOUND") # Not needed
-        set(LINK_LIBS "")
+    if(LINK_LIBS)
+        # Workaround a mechanism described in (https://cmake.org/cmake/help/v3.30/prop_tgt/INTERFACE_LINK_LIBRARIES.html)
+        list(FILTER LINK_LIBS EXCLUDE REGEX "::@")
+        list(REMOVE_DUPLICATES LINK_LIBS)
     endif()
 
-    # Workaround a mechanism described in (https://cmake.org/cmake/help/v3.30/prop_tgt/INTERFACE_LINK_LIBRARIES.html)
-    list(FILTER LINK_LIBS EXCLUDE REGEX "::@")
-
-    # message("LINK LIBS for lib: ${NODE} are: ${LINK_LIBS}")
-    foreach(lib ${LINK_LIBS})
-        __flatten_graph_recursive(${lib} LIB_ADDED)
-        if(LIB_ADDED EQUAL 1)
-            __append_rm_list_unique(${NODE} ${lib})
-            get_target_property(_RM_LIST ${NODE} __RM_LIST )
-        endif()
+    # Visit each child recursively
+    foreach(child ${LINK_LIBS})
+        alias_dereference(child ${child})
+        __dfs_topo(${child} _child_ret)
     endforeach()
 
-    set(${RET} 0 PARENT_SCOPE)
-endfunction()
+    # Mark node as processed
+    set_property(TARGET ${NODE} PROPERTY __ALREADY_VISITED FALSE)
+    set_property(TARGET ${NODE} PROPERTY __NODE_PROCESSED TRUE)
 
-function(__append_rm_list_unique NODE RM_EL) # Append if element not already in list
-    alias_dereference(NODE ${NODE})
-    get_target_property(RM_LIST ${NODE} __RM_LIST )
-
-    list(FIND RM_LIST ${RM_EL} FOUND)
-    if(FOUND EQUAL -1)
-        set_property(TARGET ${NODE} APPEND PROPERTY __RM_LIST ${RM_EL})
+    # Append node to global stack if not already added
+    get_property(STACK GLOBAL PROPERTY __GLOBAL_STACK)
+    list(FIND STACK ${NODE} ALREADY_ADDED)
+    if(ALREADY_ADDED EQUAL -1)
+        list(APPEND STACK ${NODE})
+        set_property(GLOBAL PROPERTY __GLOBAL_STACK ${STACK})
     endif()
+
+    set(${RET} 1 PARENT_SCOPE)
 endfunction()
 
+# ------------------------------------------------------------------ #
+# Utility: compare lists (unchanged)
+# ------------------------------------------------------------------ #
 function(compare_lists L1 L2 RET)
-    list(LENGTH L1 L1_LEN)
-    list(LENGTH L2 L2_LEN)
+    set(_l1 ${L1})
+    set(_l2 ${L2})
+    list(LENGTH _l1 L1_LEN)
+    list(LENGTH _l2 L2_LEN)
 
     if(NOT (L1_LEN EQUAL L2_LEN))
         set(${RET} -1 PARENT_SCOPE)
         return()
     endif()
 
-    foreach(el ${L1})
-        list(FIND L2 ${el} FOUND)
-        if(FOUND EQUAL -1)
-            set(${RET} -1 PARENT_SCOPE)
-            return()
-        endif()
-    endforeach()
+    list(SORT _l1)
+    list(SORT _l2)
+
+    if(NOT "${_l1}" STREQUAL "${_l2}")
+        set(${RET} -1 PARENT_SCOPE)
+        return()
+    endif()
 
     set(${RET} 1 PARENT_SCOPE)
 endfunction()
-
-function(__all_vertices_removed NODE RET)
-    if(NOT TARGET ${NODE})
-        message(FATAL_ERROR "Node is not defined ${NODE}")
-    endif()
-    get_target_property(RM_LIST ${NODE} __RM_LIST)
-    get_target_property(LINK_LIBS ${NODE} INTERFACE_LINK_LIBRARIES)
-    list(REMOVE_DUPLICATES LINK_LIBS)
-
-    if(RM_LIST STREQUAL "RM_LIST-NOTFOUND")
-        set(RM_LIST "")
-    endif()
-    if(LINK_LIBS STREQUAL "LINK_LIBS-NOTFOUND")
-        set(LINK_LIBS "")
-    endif()
-    # Workaround a mechanism described in (https://cmake.org/cmake/help/v3.30/prop_tgt/INTERFACE_LINK_LIBRARIES.html)
-    list(FILTER LINK_LIBS EXCLUDE REGEX "::@")
-
-    compare_lists("${RM_LIST}" "${LINK_LIBS}" L_EQ)
-    set(${RET} ${L_EQ} PARENT_SCOPE)
-endfunction()
-
-
 
