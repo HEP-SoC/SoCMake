30925f33803d9ae170c817dfacc9cfed244de168
Risto Pejasinovic <risto.pejasinovic@gmail.com>
2026-01-24 23:56:04 +0100

    Support different capitalization variants in find_package()

--- bootstrap/socmake-config.cmake
+++ bootstrap/socmake-config.cmake
@@ -1,17 +1,17 @@
-set(_socmake_version "${SoCMake_FIND_VERSION}")
+set(_socmake_version "${${CMAKE_FIND_PACKAGE_NAME}_FIND_VERSION}")
 set(_socmake_git_ref "develop")
 set(_socmake_git_url "https://github.com/HEP-SoC/SoCMake.git")
 
 if(_socmake_version)
     # If version is passed in find_package() call, use it as tag v<VERSION> (prepended v)
     set(_socmake_git_ref "v${_socmake_version}")
-elseif(DEFINED SoCMake_GIT_TAG)
+elseif(DEFINED SOCMAKE_GIT_TAG)
     # Allow custom variable for tag/branch/commit
-    set(_socmake_git_ref "${SoCMake_GIT_TAG}")
+    set(_socmake_git_ref "${SOCMAKE_GIT_TAG}")
 endif()
 
 # Git url can be changed with SOCMAKE_GIT_URL variable
-if(SOCMAKE_GIT_URL)
+if(DEFINED SOCMAKE_GIT_URL)
     set(_socmake_git_url "${SOCMAKE_GIT_URL}")
 endif()
 
@@ -20,13 +20,14 @@ endif()
 set(CMAKE_POLICY_DEFAULT_CMP0168 NEW)
 
 include(FetchContent)
-FetchContent_Declare(SoCMake
+FetchContent_Declare(${CMAKE_FIND_PACKAGE_NAME}
     GIT_REPOSITORY ${_socmake_git_url}
     GIT_TAG ${_socmake_git_ref}
 )
 FetchContent_MakeAvailable(SoCMake)
 
-set(SoCMake_FOUND TRUE)
-set(SoCMake_VERSION ${SoCMake_FIND_VERSION})
+set(${CMAKE_FIND_PACKAGE_NAME}_FOUND TRUE)
+set(${CMAKE_FIND_PACKAGE_NAME}_VERSION ${${CMAKE_FIND_PACKAGE_NAME}_FIND_VERSION})
+
 # Override the variable that find_package() sets to point to the fetched repo instead of the one in .local/lib/cmake
-set(SoCMake_DIR "${SoCMake_SOURCE_DIR}" CACHE STRING "SoCMake package dir" FORCE)
+set(${CMAKE_FIND_PACKAGE_NAME}_DIR "${socmake_SOURCE_DIR}" CACHE STRING "SoCMake package dir" FORCE)
