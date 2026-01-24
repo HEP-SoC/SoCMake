3ac86d7f58dbcfcd475e20ed41669465d2881208
Risto Pejasinovic <risto.pejasinovic@gmail.com>
2026-01-24 23:26:00 +0100

    Small fixes in the bootstrap script

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 
 # TODO We should fix this to a tag that we know contains these scripts
@@ -8,7 +8,7 @@ INSTALL_DIR="${HOME}/.local/lib/cmake/socmake"
 # TODO change this once merged to main repo
 REPO_URL="${SOCMAKE_REPO_URL:-https://raw.githubusercontent.com/risto97/socmake}"
 
-echo "Installing SoCmake bootstrap files..."
+echo "Installing SoCMake bootstrap files..."
 echo "  Git tag: ${SOCMAKE_VERSION}"
 echo "  Install directory: ${INSTALL_DIR}"
 echo "  Repository: ${REPO_URL}"
@@ -17,14 +17,14 @@ echo "  Repository: ${REPO_URL}"
 mkdir -p "${INSTALL_DIR}"
 
 # Download bootstrap files
-echo "Downloading bootstrap files..."
+echo "Downloding bootstrap files..."
 curl -fsSL "${REPO_URL}/${SOCMAKE_VERSION}/bootstrap/socmake-config.cmake" \
   -o "${INSTALL_DIR}/socmake-config.cmake"
 
 curl -fsSL "${REPO_URL}/${SOCMAKE_VERSION}/bootstrap/socmake-config-version.cmake" \
   -o "${INSTALL_DIR}/socmake-config-version.cmake"
 
-echo "✓ SoCmake bootstrap installed successfully! in ${INSTALL_DIR}"
+echo "✓ SoCMake bootstrap installed successfully! in ${INSTALL_DIR}"
 echo ""
 echo "To use in your CMakeLists.txt add the following:"
 echo "  find_package(socmake)"
