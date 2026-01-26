254645ae53c965c3dffa557b28e6cddf4bce51b8
mtravaillard <maxim.travaillard@gmail.com>
2026-01-26 10:34:33 +0100

    Update test.sh for questa support

--- examples/simple_mixed_language_sc_vlog/test.sh
+++ examples/simple_mixed_language_sc_vlog/test.sh
@@ -1,17 +1,37 @@
-cmake -DSIMULATOR=mgc -DTEST_LANG=systemc -DDUT_LANG=systemc  -S $(pwd) -B $(pwd)/build
+# Modelsim
+
+cmake -DSIMULATOR=modelsim -DTEST_LANG=systemc -DDUT_LANG=systemc  -S $(pwd) -B $(pwd)/build
 cmake --build $(pwd)/build --target run_test_counters_modelsim
 rm -rf build
 
-cmake -DSIMULATOR=mgc -DTEST_LANG=systemc -DDUT_LANG=verilog  -S $(pwd) -B $(pwd)/build
+cmake -DSIMULATOR=modelsim -DTEST_LANG=systemc -DDUT_LANG=verilog  -S $(pwd) -B $(pwd)/build
 cmake --build $(pwd)/build --target run_test_counters_modelsim
 rm -rf build
 
-cmake -DSIMULATOR=mgc -DTEST_LANG=verilog -DDUT_LANG=verilog  -S $(pwd) -B $(pwd)/build
+cmake -DSIMULATOR=modelsim -DTEST_LANG=verilog -DDUT_LANG=verilog  -S $(pwd) -B $(pwd)/build
 cmake --build $(pwd)/build --target run_test_counters_modelsim
 rm -rf build
 
-cmake -DSIMULATOR=mgc -DTEST_LANG=verilog -DDUT_LANG=systemc  -S $(pwd) -B $(pwd)/build
+cmake -DSIMULATOR=modelsim -DTEST_LANG=verilog -DDUT_LANG=systemc  -S $(pwd) -B $(pwd)/build
 cmake --build $(pwd)/build --target run_test_counters_modelsim
+rm -rf build
+
+# Questasim
+
+cmake -DSIMULATOR=questa -DTEST_LANG=systemc -DDUT_LANG=systemc  -S $(pwd) -B $(pwd)/build
+cmake --build $(pwd)/build --target run_test_counters_questasim
+rm -rf build
+
+cmake -DSIMULATOR=questa -DTEST_LANG=systemc -DDUT_LANG=verilog  -S $(pwd) -B $(pwd)/build
+cmake --build $(pwd)/build --target run_test_counters_questasim
+rm -rf build
+
+cmake -DSIMULATOR=questa -DTEST_LANG=verilog -DDUT_LANG=verilog  -S $(pwd) -B $(pwd)/build
+cmake --build $(pwd)/build --target run_test_counters_questasim
+rm -rf build
+
+cmake -DSIMULATOR=questa -DTEST_LANG=verilog -DDUT_LANG=systemc  -S $(pwd) -B $(pwd)/build
+cmake --build $(pwd)/build --target run_test_counters_questasim
 rm -rf build
 
 # Xcelium
