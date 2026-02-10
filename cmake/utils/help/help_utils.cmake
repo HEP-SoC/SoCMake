974c0ebba539f96497b8ab27627e450fbec5420e
Risto Pejasinovic <risto.pejasinovic@gmail.com>
2026-02-10 22:05:33 +0100

    Bring back PRINT_ON_CONF

--- cmake/utils/help/help_utils.cmake
+++ cmake/utils/help/help_utils.cmake
@@ -11,7 +11,52 @@ function(__cmake_to_json_array OUTVAR)
     set(${OUTVAR} ${values_array} PARENT_SCOPE)
 endfunction()
 
+function(_create_help_target HELP_NAME JQ_FILE OUTFILE GROUP_NAME)
+    cmake_parse_arguments(ARG "PRINT_ON_CONF" "" "" ${ARGN})
+    if(ARG_UNPARSED_ARGUMENTS)
+        message(FATAL_ERROR "${CMAKE_CURRENT_FUNCTION} passed unrecognized argument " "${ARG_UNPARSED_ARGUMENTS}")
+    endif()
+
+    set(target help_${HELP_NAME})
+
+    set(cmd jq -L ${CMAKE_CURRENT_FUNCTION_LIST_DIR} -r
+               -f ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/${JQ_FILE} ${outfile})
+    set(DESCRIPTION "Help for ${HELP_NAME}")
+    add_custom_target(${target}
+        COMMAND ${cmd} --argjson termwidth \"$$\(tput cols\)\" --arg group \"${GROUP_NAME}\"
+        COMMENT ${DESCRIPTION}
+        )
+    set_property(TARGET ${target} PROPERTY DESCRIPTION ${DESCRIPTION})
+    set_property(TARGET ${target} APPEND PROPERTY SOCMAKE_GROUPS help)
+
+    if(ARG_PRINT_ON_CONF)
+        execute_process(COMMAND tput cols
+            OUTPUT_VARIABLE TERM_WIDTH
+            OUTPUT_STRIP_TRAILING_WHITESPACE
+            RESULT_VARIABLE tput_result)
+        if(tput_result)
+            message(WARNING "tput failed with code ${tput_result}, using default width")
+            set(TERM_WIDTH 150)
+        endif()
+
+        execute_process(
+            COMMAND ${cmd} --argjson termwidth ${TERM_WIDTH} --arg group "${GROUP_NAME}"
+        )
+    endif()
+
+
+endfunction()
+
 function(help_options)
+    cmake_parse_arguments(ARG "PRINT_ON_CONF" "" "" ${ARGN})
+    if(ARG_UNPARSED_ARGUMENTS)
+        message(FATAL_ERROR "${CMAKE_CURRENT_FUNCTION} passed unrecognized argument " "${ARG_UNPARSED_ARGUMENTS}")
+    endif()
+
+    if(ARG_PRINT_ON_CONF)
+        set(ARG_PRINT_ON_CONF PRINT_ON_CONF)
+    endif()
+
     get_property(ALL_OPTIONS GLOBAL PROPERTY SOCMAKE_OPTIONS)
     if(NOT ALL_OPTIONS)
         return()
@@ -52,20 +97,21 @@ function(help_options)
     set(outfile ${PROJECT_BINARY_DIR}/help/${target}.json)
     file(WRITE ${outfile} ${json_output})
 
-    set(cmd jq -L ${CMAKE_CURRENT_FUNCTION_LIST_DIR} -r
-               --argjson termwidth \"$$\(tput cols\)\"
-               -f ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/option.jq ${outfile})
-    set(DESCRIPTION "Help for Options")
-    add_custom_target(${target}
-        COMMAND ${cmd}
-        COMMENT ${DESCRIPTION}
-        )
-    set_property(TARGET ${target} PROPERTY DESCRIPTION ${DESCRIPTION})
-    set_property(TARGET ${target} APPEND PROPERTY SOCMAKE_GROUPS help)
+    _create_help_target("options" "option.jq" ${outfile} "*" ${ARG_PRINT_ON_CONF})
+
 endfunction()
 
 
 function(help_ips)
+    cmake_parse_arguments(ARG "PRINT_ON_CONF" "" "" ${ARGN})
+    if(ARG_UNPARSED_ARGUMENTS)
+        message(FATAL_ERROR "${CMAKE_CURRENT_FUNCTION} passed unrecognized argument " "${ARG_UNPARSED_ARGUMENTS}")
+    endif()
+
+    if(ARG_PRINT_ON_CONF)
+        set(ARG_PRINT_ON_CONF PRINT_ON_CONF)
+    endif()
+
     get_all_ips(ALL_IPS)
     # if(NOT ALL_IPS)
     #     return()
@@ -98,34 +144,24 @@ function(help_ips)
     set(outfile ${PROJECT_BINARY_DIR}/help/${target}.json)
     file(WRITE ${outfile} ${json_output})
 
-    set(cmd jq -L ${CMAKE_CURRENT_FUNCTION_LIST_DIR} -r 
-               --argjson termwidth \"$$\(tput cols\)\"
-               -f ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/ip.jq ${outfile})
-    set(DESCRIPTION "Help for IPs")
-    add_custom_target(${target}
-        COMMAND ${cmd}
-        COMMENT ${DESCRIPTION}
-        )
-    set_property(TARGET ${target} PROPERTY DESCRIPTION ${DESCRIPTION})
-    set_property(TARGET ${target} APPEND PROPERTY SOCMAKE_GROUPS help)
+    _create_help_target("ips" "ip.jq" ${outfile} "*" ${ARG_PRINT_ON_CONF})
 endfunction()
 
 function(help_targets)
+    cmake_parse_arguments(ARG "PRINT_ON_CONF" "" "" ${ARGN})
+    if(ARG_UNPARSED_ARGUMENTS)
+        message(FATAL_ERROR "${CMAKE_CURRENT_FUNCTION} passed unrecognized argument " "${ARG_UNPARSED_ARGUMENTS}")
+    endif()
+
+    if(ARG_PRINT_ON_CONF)
+        set(ARG_PRINT_ON_CONF PRINT_ON_CONF)
+    endif()
 
     # Create the target first, so it can be discovered by get_all_targets() function
     set(target help_targets)
     set(outfile ${PROJECT_BINARY_DIR}/help/${target}.json)
 
-    set(cmd jq -L ${CMAKE_CURRENT_FUNCTION_LIST_DIR} -r 
-               --argjson termwidth \"$$\(tput cols\)\"
-               -f ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/target.jq ${outfile})
-    set(DESCRIPTION "Help for targets")
-    add_custom_target(${target}
-        COMMAND ${cmd}
-        COMMENT ${DESCRIPTION}
-        )
-    set_property(TARGET ${target} PROPERTY DESCRIPTION ${DESCRIPTION})
-    set_property(TARGET ${target} APPEND PROPERTY SOCMAKE_GROUPS help)
+    _create_help_target("targets" "target.jq" ${outfile} "*" ${ARG_PRINT_ON_CONF})
 
     # Now get all the targets and write to JSON
     get_all_targets(ALL_TARGETS)
@@ -164,9 +200,13 @@ endfunction()
 
 
 function(help_custom_targets GROUP_NAME)
-    cmake_parse_arguments(ARG "DONT_MAKE_GROUP" "PATTERN;DESCRIPTION;HELP_TARGET_NAME" "LIST" ${ARGN})
+    cmake_parse_arguments(ARG "DONT_MAKE_GROUP;PRINT_ON_CONF" "PATTERN;DESCRIPTION;HELP_TARGET_NAME" "LIST" ${ARGN})
     if(ARG_UNPARSED_ARGUMENTS)
         message(FATAL_ERROR "${CMAKE_CURRENT_FUNCTION} passed unrecognized argument " "${ARG_UNPARSED_ARGUMENTS}")
+    endif()
+
+    if(ARG_PRINT_ON_CONF)
+        set(ARG_PRINT_ON_CONF PRINT_ON_CONF)
     endif()
 
     if(NOT DEFINED ARG_DESCRIPTION)
@@ -185,32 +225,25 @@ function(help_custom_targets GROUP_NAME)
     endif()
 
     if(DEFINED ARG_HELP_TARGET_NAME)
-        set(target help_${ARG_HELP_TARGET_NAME})
+        set(help_name ${ARG_HELP_TARGET_NAME})
     else()
-        set(target help_${GROUP_NAME})
+        set(help_name ${GROUP_NAME})
     endif()
 
     set(outfile ${PROJECT_BINARY_DIR}/help/help_targets.json)
 
-    set(cmd jq -L ${CMAKE_CURRENT_FUNCTION_LIST_DIR} -r
-               --arg group \"${GROUP_NAME}\"
-               --argjson termwidth \"$$\(tput cols\)\"
-               -f ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/target.jq ${outfile})
-    add_custom_target(${target}
-        COMMAND ${cmd}
-        COMMENT ${ARG_DESCRIPTION}
-        )
-    set_property(TARGET ${target} PROPERTY DESCRIPTION ${ARG_DESCRIPTION})
-    set_property(TARGET ${target} APPEND PROPERTY SOCMAKE_GROUPS help)
-
+    _create_help_target("${help_name}" "target.jq" ${outfile} "${GROUP_NAME}" ${ARG_PRINT_ON_CONF})
 endfunction()
 
 function(help_custom_ips GROUP_NAME)
-    cmake_parse_arguments(ARG "" "PATTERN;DESCRIPTION" "LIST" ${ARGN})
+    cmake_parse_arguments(ARG "PRINT_ON_CONF" "PATTERN;DESCRIPTION" "LIST" ${ARGN})
     if(ARG_UNPARSED_ARGUMENTS)
         message(FATAL_ERROR "${CMAKE_CURRENT_FUNCTION} passed unrecognized argument " "${ARG_UNPARSED_ARGUMENTS}")
     endif()
 
+    if(ARG_PRINT_ON_CONF)
+        set(ARG_PRINT_ON_CONF PRINT_ON_CONF)
+    endif()
 
     if(NOT DEFINED ARG_DESCRIPTION)
         set(ARG_DESCRIPTION "Help for ${GROUP_NAME} IPs")
@@ -225,28 +258,21 @@ function(help_custom_ips GROUP_NAME)
 
     group_custom_ips("${GROUP_NAME}" ${ARG_PATTERN} ${ARG_LIST})
 
-    set(target help_${GROUP_NAME})
     set(outfile ${PROJECT_BINARY_DIR}/help/help_ips.json)
 
-    set(cmd jq -L ${CMAKE_CURRENT_FUNCTION_LIST_DIR} -r
-               --arg group \"${GROUP_NAME}\"
-               --argjson termwidth \"$$\(tput cols\)\"
-               -f ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/ip.jq ${outfile})
-    add_custom_target(${target}
-        COMMAND ${cmd}
-        COMMENT ${ARG_DESCRIPTION}
-        )
-    set_property(TARGET ${target} PROPERTY DESCRIPTION ${ARG_DESCRIPTION})
-    set_property(TARGET ${target} APPEND PROPERTY SOCMAKE_GROUPS help)
+    _create_help_target("${GROUP_NAME}" "ip.jq" ${outfile} "${GROUP_NAME}" ${ARG_PRINT_ON_CONF})
 
 endfunction()
 
 function(help_custom_options GROUP_NAME)
-    cmake_parse_arguments(ARG "" "PATTERN;DESCRIPTION" "LIST" ${ARGN})
+    cmake_parse_arguments(ARG "PRINT_ON_CONF" "PATTERN;DESCRIPTION" "LIST" ${ARGN})
     if(ARG_UNPARSED_ARGUMENTS)
         message(FATAL_ERROR "${CMAKE_CURRENT_FUNCTION} passed unrecognized argument " "${ARG_UNPARSED_ARGUMENTS}")
     endif()
 
+    if(ARG_PRINT_ON_CONF)
+        set(ARG_PRINT_ON_CONF PRINT_ON_CONF)
+    endif()
 
     if(NOT DEFINED ARG_DESCRIPTION)
         set(ARG_DESCRIPTION "Help for ${GROUP_NAME} Options")
@@ -261,19 +287,9 @@ function(help_custom_options GROUP_NAME)
 
     group_custom_options("${GROUP_NAME}" ${ARG_PATTERN} ${ARG_LIST})
 
-    set(target help_${GROUP_NAME})
     set(outfile ${PROJECT_BINARY_DIR}/help/help_options.json)
 
-    set(cmd jq -L ${CMAKE_CURRENT_FUNCTION_LIST_DIR} -r
-               --arg group \"${GROUP_NAME}\"
-               --argjson termwidth \"$$\(tput cols\)\"
-               -f ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/option.jq ${outfile})
-    add_custom_target(${target}
-        COMMAND ${cmd}
-        COMMENT ${ARG_DESCRIPTION}
-        )
-    set_property(TARGET ${target} PROPERTY DESCRIPTION ${ARG_DESCRIPTION})
-    set_property(TARGET ${target} APPEND PROPERTY SOCMAKE_GROUPS help)
+    _create_help_target("${GROUP_NAME}" "option.jq" ${outfile} "${GROUP_NAME}" ${ARG_PRINT_ON_CONF})
 
 endfunction()
 
