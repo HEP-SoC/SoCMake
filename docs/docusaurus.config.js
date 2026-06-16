31536e4742c405b2b6e9eb3cfc06ca7cf4522e1b
Benoit Denkinger <benoit.denkinger@cern.ch>
2026-06-16 10:48:36 +0200

    docs: updated docusaurus to v3.10.1
    - migrate onBrokenMarkdownLinks to markdown.hooks (deprecated top-level option)
    - bump engines.node to >=20.0 to match Docusaurus 3.10.1 requirement
    - drop now-unneeded webpack override (3.10.1 ships a compatible webpack)
    - pin Node 20 via actions/setup-node in pages.yml CI workflow

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -34,7 +34,12 @@ const config = {
   projectName: 'socmake', // Usually your repo name.
 
   onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'throw',
+
+  markdown: {
+    hooks: {
+      onBrokenMarkdownLinks: 'throw',
+    },
+  },
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
